9eeb8670eda0ff4000f1955e0f59ae6eb63f3227
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-03-06 21:28:32 +0700

    number in woords

--- number_in_words.js
+++ number_in_words.js
@@ -1,8 +1,28 @@
-function numberToWords(number) {
-  // Your code here
+function ubah(num){
+var angka=[1,2,3,4,5,6,7,8,9]
+var kamus=['satu ','dua ','tiga ','empat ','lima ','enam ','tujuh ','delapan ','sembilan ']
+var hasil='';
+for (var i = 0; i < angka.length; i++) {
+  for (var j = 0; j < kamus.length; j++) {
+    // if(num > 10){
+    //   hasil+= kamus[j]
+    }
+    if(num == angka[i]){
+      hasil += kamus[i]
+    }else if(num/10==angka[i]){
+      hasil+= kamus[i]+'puluh'
+    }else if(Math.floor(num/10)!==0 && ( num%10 )== angka[i]){
+      hasil+= kamus[i]+' belas'
+    }
+  }
+return hasil;
 }
 
-// Driver code
-console.log(numberToWords(705));
-console.log(numberToWords(1000000));
-console.log(numberToWords(2011845));
+
+
+
+console.log(ubah(4));
+console.log(ubah(15))
+console.log(ubah(50))
+console.log(ubah(70))
+console.log(ubah(90))
